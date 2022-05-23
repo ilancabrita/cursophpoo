e1481b9a98d5243bfe61c94ca7f7318c427271bd
ilancabrita <82396649+ilancabrita@users.noreply.github.com>
2022-05-23 20:43:00 -0300

    maj 3 cours 22.05.23

--- 21-crud-com-pdo_singleton-autoload/index.php
+++ 21-crud-com-pdo_singleton-autoload/index.php
@@ -1,5 +1,6 @@
 <?php
 // CRUD com PDO, Singleton e autoload
+
 require_once 'vendor/autoload.php';
 
 $produto = new \App\Model\Produto();
@@ -7,13 +8,18 @@ $produto->setId(1);
 $produto->setNome("Notebook DELL");
 $produto->setDescricao('i5, 4gb');
 
+$produto2 = new \App\Model\Produto();
+$produto2->setNome("Notebook HP");
+$produto2->setDescricao('i7, 16gb');
+
 var_dump($produto);
 
 $produtoDao = new \App\Model\ProdutoDao();
 $produtoDao->create($produto);
-$produtoDao->read();
-$produtoDao->update($produto);
-$produtoDao->delete(2);
+$produtoDao->create($produto2);
+//$produtoDao->read();
+//$produtoDao->update($produto);
+//$produtoDao->delete(2);
 
 foreach($produtoDao->read() as $produto):
     echo $produto['nome']."<br>".$produto['descricao']."<hr>";
